b4178b060aa9ec72032ccfbacfd3f4538f28d7ed
skoopplayz <157137471+skoopplayz@users.noreply.github.com>
2025-11-12 20:16:51 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/belugas/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://wisp.rhw.one',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
